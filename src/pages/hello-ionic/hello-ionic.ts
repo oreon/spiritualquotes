7868c074550dab81b76f21a1228e13bdf19fde07
onkar singh <onkarsinghmnit@gmail.com>
2019-01-19 01:18:31 +0530

    New algo with 1 notif for tomorrow and other improvements

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -5,6 +5,7 @@ import { BaseEntity } from "../../base/base-entity";
 import { BaseFireService } from "../../base/BaseFireService";
 import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import { SettingsProvider } from "../../providers/settings/settings";
+import { LocalNotifications } from "@ionic-native/local-notifications";
 
 export interface Quote extends BaseEntity {
   text: string;
@@ -19,20 +20,36 @@ export class HelloIonicPage {
   public settingsForm: FormGroup;
 
   constructor(
+    public platform: Platform,    
+    private localNotifications: LocalNotifications,
     public navCtrl: NavController,
     private fireService: BaseFireService<Quote>,
     public plt: Platform,
     private fb: FormBuilder,
     private settings: SettingsProvider
   ) {
-    this.fireService.getRecords().subscribe(x => (this.items = x));
     this.createForm();
 
     this.settings.getSettings().then(x => {
-      this.settingsForm.get("start").setValue(x["start"]);
-      this.settingsForm.get("end").setValue(x["end"]);
-      this.settingsForm.get("frequency").setValue(x["frequency"]);
+      console.log(this.settingsForm.value.start);
+      console.log("X - ", x);
+      if(x){
+        this.settingsForm.get("start").setValue(x["start"]);
+        this.settingsForm.get("end").setValue(x["end"]);
+        this.settingsForm.get("frequency").setValue(x["frequency"]);
+      }
     });
+    this.platform.ready().then(() => {
+      if(this.platform.is('android')){
+        console.log("Hello ionic file mein chal raha");
+        this.localNotifications.getAll()
+        .then(x => {
+          console.log(x);
+          console.log(x.length);
+          this.items = x;
+        })
+      }
+    })
   }
 
   save() {
